b26e3613de5843f0e9a25386c88236d4ba8d7a19
MrG <bart@exaactly.com>
2018-01-18 22:15:31 +0000

    Added gitignore

--- public/bundle.js
+++ public/bundle.js
@@ -957,19 +957,6 @@ var template = _react2.default.createElement(
 );
 _reactDom2.default.render(template, document.getElementById('app'));
 
-// import subtract, {square, add} from './utils.jsx';
-
-// console.log('app.jsx is running!@');
-// console.log(square(4));
-// console.log(add(44,15));
-// console.log(subtract(45,15));
-
-//
-// import isSenior, {isAdult, canDrink} from './person.jsx';
-// console.log(isAdult(18));
-// console.log(canDrink(18));
-// console.log(isSenior(64));
-
 /***/ }),
 /* 15 */
 /***/ (function(module, exports, __webpack_require__) {
